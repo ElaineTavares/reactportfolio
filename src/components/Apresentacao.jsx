eacabf55164cc0d23be9268a5e30f8a0ab958bec
Elaine Tavares <elainetavares.dev@gmail.com>
2024-06-19 15:51:22 -0300

    Apresentacao

--- src/components/Apresentacao.jsx
+++ src/components/Apresentacao.jsx
@@ -8,7 +8,7 @@ export default function Apresentacao() {
         <div className='container'>
             <h2>Bem-vindo ao meu Portfólio!</h2>
             <div className='cursor'></div>
-            <h1>Olá, me chamo Elaine Tavares, <br /> sou <span>Desenvolvedora front End</span></h1>
+            <h1>Olá, me chamo <span>Elaine Tavares,</span></h1>
             <p>Formada em Tecnologia em Gestão Financeira, me apaixonei pelo Desenvolvimento Web e busco transição de carreira para a área, possuo competência em GitHub, HTML, CSS, JavaScript e React e estou atuando como voluntária em Desenvolvimento Front-end no Instituto Capoeira Social.</p>
             <Botao texto="Currículo" link="https://drive.google.com/file/d/1E2bOurI_JM86VMMV4oZa1Y0ji2N4MsUt/view?usp=drive_link"/>
         </div>   
