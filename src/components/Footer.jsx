0748e11b3fd96f4d6823beb5b80e18619be5b4ad
Elaine Tavares <elainetavares.dev@gmail.com>
2024-09-05 08:44:01 -0300

    link do linkedin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,7 +25,7 @@ export default function Footer() {
                 </div>
             </div>
             <div className="redes">
-            <a href="www.linkedin.com/in/elaine-tavares-b5797b1b0" target='blank'><img className='linkedin' src={linkedin} alt="linkedin" /></a>
+            <a href="https://www.linkedin.com/in/elaine-tavares-b5797b1b0/" target='blank'><img className='linkedin' src={linkedin} alt="linkedin" /></a>
             <a href="https://github.com/ElaineTavares" target='blank'><img className='github' src={github} alt="github" /></a>
             <a href="https://api.whatsapp.com/send/?phone=5521984510746&text=Ol%C3%A1%2C+desejo+informa%C3%A7%C3%A3o+sobre+seu+serv&type=phone_number&app_absent=0" target='blank'><img className='whats' src={whats} alt="whatsapp" /></a>
             </div>
